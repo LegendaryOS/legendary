f73ab15686369eccef8138601556b95bd13599d1
michal92299 <chudybyczek2137@gmail.com>
2025-10-27 15:10:50 +0100

    Create remove.js

---
+++ atomic/commands/remove.js
@@ -0,0 +1,27 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const { createSnapshot, prepareChroot, runInChroot, cleanupChroot, deploySnapshot } = require('../utils/btrfs');
+
+module.exports = (pkg) => {
+  console.log(chalk.green(`Removing package: ${pkg}`));
+
+  const timestamp = new Date().toISOString().replace(/[:.-]/g, '');
+  const snapDir = `/var/lib/legendary/snapshots/${timestamp}`;
+  const snapRoot = path.join(snapDir, '@');
+
+  try {
+    fs.mkdirSync(snapDir, { recursive: true });
+    createSnapshot(snapRoot);
+    prepareChroot(snapRoot);
+    runInChroot(snapRoot, `pacman -R --noconfirm ${pkg}`);
+    cleanupChroot(snapRoot);
+    deploySnapshot(snapRoot);
+    console.log(chalk.green('Removal complete. Reboot to apply changes.'));
+  } catch (error) {
+    console.error(chalk.red(`Error during remove: ${error.message}`));
+    execSync(`btrfs subvolume delete ${snapRoot}`);
+    fs.rmdirSync(snapDir);
+  }
+};
