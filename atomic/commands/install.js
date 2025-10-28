9ed3577adb1502855d6a127ef2d02f703ff2c0a8
michal92299 <chudybyczek2137@gmail.com>
2025-10-28 14:24:50 +0100

    Update install.js

--- atomic/commands/install.js
+++ atomic/commands/install.js
@@ -4,6 +4,8 @@ const path = require('path');
 const chalk = require('chalk');
 const { createSnapshot, prepareChroot, runInChroot, cleanupChroot, deploySnapshot } = require('../utils/btrfs');
 
+const PACMAN_PATH = '/usr/lib/LegendaryOS/pacman';
+
 module.exports = (pkg) => {
   console.log(chalk.green(`Installing package: ${pkg}`));
 
@@ -15,7 +17,7 @@ module.exports = (pkg) => {
     fs.mkdirSync(snapDir, { recursive: true });
     createSnapshot(snapRoot);
     prepareChroot(snapRoot);
-    runInChroot(snapRoot, `pacman -S --noconfirm ${pkg}`);
+    runInChroot(snapRoot, `${PACMAN_PATH} -S --noconfirm ${pkg}`);
     cleanupChroot(snapRoot);
     deploySnapshot(snapRoot);
     console.log(chalk.green('Installation complete. Reboot to apply changes.'));
